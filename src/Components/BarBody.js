9215e05546e11983c3966a20d30ae020fab185be
Maksym <draganovmaksim9@gmail.com>
2021-03-13 18:13:24 +0200

    first layout version

--- src/Components/BarBody.js
+++ src/Components/BarBody.js
@@ -1,18 +1,16 @@
 import React from 'react'
 
-const BarBody = ({ percent, type, label }) => {
+const BarBody = ({ percent, type, label, direction }) => {
 	return (
 		<div className="bar">
 			<div className={"bar__body " + type} style={{ height: percent * 4 }}>
 				<label className="bar__body_label" >{label}</label>
 				<div className="bar__body_arrow" >
-					<div className="bar__body_arrow__direction down">
-
-					</div>
-					<div className="bar__body_arrow__percentage ">-25%</div>
+					<div className={"bar__body_arrow__direction " + (direction > 0 ? "up" : "down")} />
+					<div className="bar__body_arrow__percentage ">{direction}%</div>
 				</div>
 			</div>
-			<div className="bar__persent">{percent}</div>
+			<div className="bar__persent">{percent}%</div>
 		</div >
 	)
 }
--- src/Components/ChartBox.js
+++ src/Components/ChartBox.js
@@ -2,12 +2,17 @@ import React from 'react'
 import BarBody from './BarBody'
 
 const ChartBox = ({ node_data }) => {
+
+
+
+	console.log(Array.from(node_data))
+
 	return (
 		<div className="chart-box">
 			<section className="chart-box__title">
 				<div className="chart-box__title_column">
 					<div className="chart-box__title_name">Flow dropout per step and service</div>
-					<div className="chart-box__title_info">i</div>
+					<div className="chart-box__title_info" />
 				</div>
 				<div className="chart-box__title_column">
 					<button className="chart-box__title_branch">Choose branches</button>
@@ -20,16 +25,61 @@ const ChartBox = ({ node_data }) => {
 					percent={100}
 					type={"basic"}
 					label={"Label"}
+					direction={-25}
 				/>
 				<BarBody
 					percent={75}
 					type={"service"}
 					label={"Label"}
+					direction={10}
 				/>
 				<BarBody
 					percent={25}
 					type={"service"}
 					label={"Label"}
+					direction={15}
+				/>
+				<BarBody
+					percent={28}
+					type={"service"}
+					label={"Label"}
+					direction={-15}
+				/>
+				<BarBody
+					percent={35}
+					type={"basic"}
+					label={"Label"}
+					direction={15}
+				/>
+				<BarBody
+					percent={100}
+					type={"service"}
+					label={"Label"}
+					direction={15}
+				/>
+				<BarBody
+					percent={25}
+					type={"service"}
+					label={"Label"}
+					direction={-15}
+				/>
+				<BarBody
+					percent={40}
+					type={"service"}
+					label={"Label"}
+					direction={55}
+				/>
+				<BarBody
+					percent={55}
+					type={"service"}
+					label={"Label"}
+					direction={15}
+				/>
+				<BarBody
+					percent={95}
+					type={"basic"}
+					label={"Label"}
+					direction={15}
 				/>
 			</section>
 
